2edc1fb27fdab60210d218d33305c8332aa38176
Sdac <Sdac@Sdac-PC>
2023-05-25 16:38:24 +0530

    update 1.0

--- Zafir/src/Zaf.java
+++ Zafir/src/Zaf.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("hello world");
 	System.out.println("new update");
 	System.out.println("update0.1");
+	System.out.println("1.0.1");
 }
 }
